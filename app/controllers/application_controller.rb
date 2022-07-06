899a89eba24da74d5e7dd94ac838ff617ac818f3
Victor <vx_zz@hotmail.com>
2022-07-06 13:46:21 -0400

    commit tree 6ef8bf1464176736b72b36fd12a3fb84d88c736a

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,18 @@
 class ApplicationController < Sinatra::Base
+  set :default_content_type, 'application/json'
 
   get '/' do
     { message: "Hello world" }.to_json
+  end
+
+  get '/games' do
+    games = Game.all.order(:title).limit(10)
+    games.to_json
+  end
+
+  get '/games/:id' do
+    game = Game.find(params[:id])
+    game.to_json(include: { reviews: { include: :user } })
   end
 
 end
